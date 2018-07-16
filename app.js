c8cca91246e84c7c17c0b12766163f2fe4deb2ea
Jaskaran Kukreja <35613378+Jaskaran121@users.noreply.github.com>
2018-07-15 22:59:27 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+//delete a book
+
+// var btns = document.querySelectorAll('#book-list ul li');
+
+// btns.forEach(function(btn)
+// {
+// 	const event = btn.addEventListener('click',function(e){
+// 		const li = e.target.parentNode;
+
+// 		li.parentNode.removeChild(li);
+// 	});
+// });
+
+//delete a book - event bubbling this is more interactive as when we add a new book we do not have to add again a eventlistener to that button.
+
+var btns = document.querySelector('#book-list ul');
+
+btns.addEventListener('click',function(e){
+	if(e.target.className=='delete')
+	{
+		const li = e.target.parentNode;
+		li.parentNode.removeChild(li);
+	}
+});
